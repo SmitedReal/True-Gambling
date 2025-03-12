8afab0b04849e37e7eeb7f4a6cdc00c04eb314f5
SmitedReal <uvojtiska@gmail.com>
2025-03-12 20:43:56 +0100

    First Commit
    Plinko unfinished

---
+++ Blackjack/Start.bat
@@ -0,0 +1,16 @@
+@echo off
+color a
+
+
+echo Thank you for using Blackjack
+echo Click to start game..
+echo ------------------------------
+echo Made by:
+echo Smite'd
+echo AND
+echo ChatGPT
+echo ------------------------------
+pause
+
+cd Assets
+java -jar BJ.jar
